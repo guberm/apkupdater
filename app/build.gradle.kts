fbae24f87b44913b897112846d7ea8a6cdc01008
Michael Guber <michael@guber.dev>
2026-08-27 23:18:31 -0400

    release: APKUpdater TV 3.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.apkupdater" + System.getenv("BUILD_TAG").orEmpty()
         minSdk = 23
         targetSdk = 36
-        versionCode = if (buildNumber.isEmpty()) 59 else buildNumber.toInt()
-        versionName = if (buildNumber.isEmpty()) "3.1.0" else "0.0.$buildNumber"
+        versionCode = if (buildNumber.isEmpty()) 60 else buildNumber.toInt()
+        versionName = if (buildNumber.isEmpty()) "3.1.1" else "0.0.$buildNumber"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
